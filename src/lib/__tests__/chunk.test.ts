ca868226aa474a8ec28d8abdd61d6532212e3c38
Pewww <jung22337@naver.com>
2019-03-03 20:22:43 +0900

    :white_check_mark: chunk 메서드 테스트 코드 작성

---
+++ src/lib/__tests__/chunk.test.ts
@@ -0,0 +1,38 @@
+import {chunk} from '../Array/chunk';
+
+describe('Array - chunk 메서드 테스트', () => {
+  test(`['a', 'b', 'c', 'd'] 배열 1개씩 chunk`, () => {
+    const arr = ['a', 'b', 'c', 'd'];
+    expect(chunk(arr, 1)).toEqual(
+      [
+        ['a'],
+        ['b'],
+        ['c'],
+        ['d']
+      ]
+    );
+  });
+
+  test(`['a', 'b', 'c', 'd'] 배열 2개씩 chunk`, () => {
+    const arr = ['a', 'b', 'c', 'd'];
+    expect(chunk(arr, 2)).toEqual(
+      [
+        ['a', 'b'],
+        ['c', 'd']
+      ]);
+  });
+
+  test(`['a', 'b', 'c', 'd'] 배열 3개씩 chunk`, () => {
+    const arr = ['a', 'b', 'c', 'd'];
+    expect(chunk(arr, 3)).toEqual(
+      [
+        ['a', 'b', 'c'],
+        ['d']
+      ]);
+  });
+
+  test(`['a', 'b', 'c', 'd'] 배열 4개씩 chunk`, () => {
+    const arr = ['a', 'b', 'c', 'd'];
+    expect(chunk(arr, 4)).toEqual([['a', 'b', 'c', 'd']]);
+  });
+});
