1bb94c991c16b43f07191291b1dbb590d5c423ac
Pewww <jung22337@naver.com>
2019-03-14 01:02:44 +0900

    :white_check_mark: Array - dropRightWhile 메서드 테스트 추가 및 일부 any 타입 인터페이스로 변경

--- src/lib/__tests__/dropRightWhile.test.ts
+++ src/lib/__tests__/dropRightWhile.test.ts
@@ -1,5 +1,15 @@
 import {dropRightWhile} from '../Array';
 
+interface IUser {
+  active: boolean;
+  name: string;
+}
+
+interface IPerson {
+  age: number;
+  name: string;
+}
+
 describe('Array - dropRightWhile 메서드 테스트', () => {
   test('첫 번째 파라미터(array)가 배열이 아닌 경우 null 반환', () => {
     const notArrayTypes = [1, 'hello', {a: 'do'}, undefined, (v: any) => v];
@@ -9,9 +19,24 @@ describe('Array - dropRightWhile 메서드 테스트', () => {
   });
 
   test('두 번째 파라미터(predicate)가 함수가 아닌 경우 배열 그대로 반환', () => {
-    const arr = [1, 2, 3, 4, 5];
+    const numArr = [1, 2, 3, 4, 5];
 
-    expect(dropRightWhile(arr, {} as any)).toEqual(arr);
+    expect(dropRightWhile(numArr, {} as any)).toEqual(numArr);
+  });
+
+  test(`파라미터가 각각 ([{age: 19, name: 'minsu'}, {age: 21, name: 'sujin'}], person => person.name)일 때 [] 반환`, () => {
+    const personArr = [
+      {
+        age: 19,
+        name: 'minsu'
+      },
+      {
+        age: 21,
+        name: 'sujin'
+      }
+    ];
+
+    expect(dropRightWhile(personArr, (person: IPerson) => person.name)).toEqual([]);
   });
 
   test('파라미터가 각각 ([1, 2, undefined, 5,undefined, 6, 6, 4], v => v)일 때, [1, 2, undefined, 5, undefined] 반환', () => {
@@ -23,7 +48,7 @@ describe('Array - dropRightWhile 메서드 테스트', () => {
   test(`파라미터가 아래 주석과 같을 때, [{active: true, user: 'barney'}] 반환`, () => {
     // Array - [{active: true, user: 'barney'}, {active: false, user: 'fred'}, {active: false, user: 'peblo'}]
     // Predicate - function (item) { return !item.active; }
-    const arr = [
+    const userArr = [
       {
         active: true,
         user: 'barney'
@@ -37,11 +62,11 @@ describe('Array - dropRightWhile 메서드 테스트', () => {
         user: 'peblo'
       }
     ];
-    const func = function(item: any) {
+    const func = function(item: IUser) {
       return !item.active;
     }
 
-    expect(dropRightWhile(arr, func)).toEqual(
+    expect(dropRightWhile(userArr, func)).toEqual(
       [{active: true, user: 'barney'}]
     );
   });
