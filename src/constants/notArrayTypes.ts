69b4a23b849787c3649a211af142ee551cc1c30a
Pewww <jung22337@naver.com>
2019-05-16 01:32:52 +0900

    :zap: notArrayTypes 상수 변경 완료

--- src/constants/notArrayTypes.ts
+++ src/constants/notArrayTypes.ts
@@ -1,10 +1,24 @@
-type TNotArrayTypes  = [
-  string
-];
+type TFunctionType = () => void;
 
-const NOT_ARRAY_TYPES: TNotArrayTypes = [
-  'Pewww'
-  // Add Later
+type TNotArrayTypes = string
+  | number
+  | boolean
+  | object
+  | null
+  | undefined
+  | TFunctionType
+  | symbol;
+
+const NOT_ARRAY_TYPES: TNotArrayTypes[] = [
+  'Pewww',
+  10,
+  NaN,
+  false,
+  {a: 1, b: 2},
+  null,
+  undefined,
+  () => { console.log('Welcome!'); },
+  Symbol('Pewww')
 ];
 
 export default NOT_ARRAY_TYPES;
