5d3458478875c13f8400685ccfd84752ed882e1a
KoreanJungYJ <jung22337@naver.com>
2019-06-13 00:45:57 +0900

    :card_index: JS Types 타입 및 상수 정의 완료

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,9 +1,41 @@
-export const STRING = 'string';
-export const ARRAY = 'array';
-export const NUMBER = 'number';
-export const BOOLEAN = 'boolean';
-export const OBJECT = 'object';
-export const NULL = 'null';
-export const UNDEFINED = 'undefined';
-export const FUNCTION = 'function';
-export const SYMBOL = 'symbol';
+export type TString = 'string';
+export type TArray = 'array';
+export type TNumber = 'number';
+export type TBoolean = 'boolean';
+export type TObject = 'object';
+export type TNull = 'null';
+export type TUndefined = 'undefined';
+export type TFunction = 'function';
+export type TSymbol = 'symbol';
+
+export const STRING: TString = 'string';
+export const ARRAY: TArray = 'array';
+export const NUMBER: TNumber = 'number';
+export const BOOLEAN: TBoolean = 'boolean';
+export const OBJECT: TObject = 'object';
+export const NULL: TNull = 'null';
+export const UNDEFINED: TUndefined = 'undefined';
+export const FUNCTION: TFunction = 'function';
+export const SYMBOL: TSymbol = 'symbol';
+
+type TFunctionType = () => void;
+
+export type TAllTypes = string
+  | any[]
+  | number
+  | boolean
+  | object
+  | null
+  | undefined
+  | TFunctionType
+  | symbol;;
+
+export type TAllTypesToString = TString
+  | TArray
+  | TNumber
+  | TBoolean
+  | TObject
+  | TNull
+  | TUndefined
+  | TFunction
+  | TSymbol;
